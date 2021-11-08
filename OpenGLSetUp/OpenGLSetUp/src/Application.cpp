8a5f04a27b7d2bd2953e6f7fab71489f00cbf001
cheredi <75421537+cheredi@users.noreply.github.com>
2021-11-08 12:04:10 +0300

    Update Application.cpp

--- OpenGLSetUp/OpenGLSetUp/src/Application.cpp
+++ OpenGLSetUp/OpenGLSetUp/src/Application.cpp
@@ -36,13 +36,48 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
         glClearColor(1, 1, 1, 1);
-
-       
-
-
-
         
 
+        glBegin(GL_POLYGON);
+        glColor3f(1.0, 1.0, 0); // Middle circle
+        double radius1 = 0.15;
+        double ori_x1 = 0.6;                         // the origin or center of circle
+        double ori_y1 = 0.7;
+        for (int i = 0; i <= 500; i++) {
+            double angle = 2 * 3.412 * i / 400;
+            double x1 = cos(angle) * radius1;
+            double y1 = sin(angle) * radius1;
+            glVertex2d(ori_x1 + x1, ori_y1 + y1);
+        }
+        glEnd();
+
+        glBegin(GL_POLYGON);
+        glColor3f(1, 1, 1); // Middle circle
+        double radius2 = 0.15;
+        double ori_x2 = 0.65;                         // the origin or center of circle
+        double ori_y2 = 0.7;
+        for (int i = 0; i <= 300; i++) {
+            double angle = 2 * 3.412 * i / 400;
+            double x2 = cos(angle) * radius2;
+            double y2 = sin(angle) * radius2;
+            glVertex2d(ori_x2 + x2, ori_y2 + y2);
+        }
+        glEnd();
+
+        glBegin(GL_LINE_LOOP);
+        glColor3f(0, 0, 0); // Middle circle
+        double radius3 = 0.4;
+        double ori_x3 = -0.07;                         // the origin or center of circle
+        double ori_y3 = 0.2;
+        for (int i = 0; i <= 300; i++) {
+            double angle = 2 * 3.412 * i / 650;
+            double x3 = cos(angle) * radius3;
+            double y3 = sin(angle) * radius3;
+            glVertex2d(ori_x3 + x3, ori_y3 + y3);
+        }
+        glEnd();
+
+       
         glBegin(GL_LINE_LOOP);
         glColor3f(0, 0, 0);
         glVertex2f(-0.8f, 0.2f);
